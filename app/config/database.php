9ba6ad4fcaec789f2a4fb48ecd625ad05e01b5ca
vkharseev <vladimir@realitygroup.ru>
2014-07-10 19:07:14 +0400

    Все готово для тестирования

--- app/config/database.php
+++ app/config/database.php
@@ -15,7 +15,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'kc_map',
 			'username'  => 'root',
-			'password'  => 'perl03',
+			'password'  => 'k.,jdmythtdyetnyfghfcyj',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
