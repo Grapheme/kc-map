7f886e4b0ffb0f44e8d0ebb3aa0719101d7fd261
vkharseev <vkharseev@gmail.com>
2014-07-25 15:35:22 +0400

    Deploy

--- app/config/github.php
+++ app/config/github.php
@@ -9,8 +9,8 @@ return array(
     'branch'            => 'master',
     'repository_name'   => 'kc-map',
     'repository_id'     => 21685216,
-    'user_group'        => 'rikardo',
-    'user_name'         => 'rikardo',
+    'user_group'        => 'www-data',
+    'user_name'         => 'www-data',
     'set_log'           => TRUE,
     'directories'       => array('/public/uploads','/app/storage')
 );
